0544bf07ea70a47a2b2b21c8c95798f1c6a50a81
Jesse <lbarbosa.jessica@gmail.com>
2020-11-17 16:16:10 -0300

    mudanças no front
    algumas pequenas mudanças no front-end

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(public loadingController: LoadingController) { }
+
+  async loadingEffect () {
+    const loading = await this.loadingController.create ({
+      message: 'Carregando Informação',
+      duration: 1000,
+      spinner: "lines"
+    });
+    await loading.present();
+    const { role, data } = await loading.onDidDismiss();
+  }
+
+    ionViewDidEnter () {
+      this.loadingEffect();
+    }
 
   ngOnInit() {
   }
