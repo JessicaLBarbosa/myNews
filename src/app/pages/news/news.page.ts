79f60f501693194efb953f4f57d8601d2bd7de5b
Jesse <lbarbosa.jessica@gmail.com>
2020-11-21 15:49:00 -0300

    mechendo na api

--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-import { IonInfiniteScroll } from '@ionic/angular';
 import { NewsService } from 'src/app/googlenews/news.service';
 import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/usuario/autenticacao.service';
@@ -13,6 +11,12 @@ import { AutenticacaoService } from 'src/app/usuario/autenticacao.service';
 })
 export class NewsPage implements OnInit {
 
+  constructor(
+    public newsService:NewsService, 
+    public autenticacaoService: AutenticacaoService,
+    public router: Router,
+  ) { }
+
   public newsList = new Array<any>();
   public page = 1;
   public maxPage:number;
@@ -20,45 +24,12 @@ export class NewsPage implements OnInit {
   public country:string = "us";
   public category:string = "science";
 
-  constructor(
-    public loadingController: LoadingController, 
-    public newsService:NewsService, 
-    public autenticacaoService: AutenticacaoService,
-    public router: Router,
-  ) { }
-
-  async loadingEffect () {
-    const loading = await this.loadingController.create ({
-      message: 'Carregando Informação',
-      duration: 1000,
-      spinner: "lines"
-    });
-    await loading.present();
-    const { role, data } = await loading.onDidDismiss();
-  }
-
-  infinityScrollEffect(event) {
-    setTimeout(() => {
-      this.page++;
-      this.loadPage();
-      console.log('Done');
-      event.target.complete();
-      if(this.page==this.maxPage){
-        event.target.disabled = true;
-      }
-    }, 4000);
-  }
-
   loadPage() {
-    this.newsService.getTopHeadlines(this.country).subscribe(
+    this.newsService.getTopHeadlines('us').subscribe(
       data => {
         const response = (data as any);
         this.newsList = this.newsList.concat(response.articles);
-        this.maxPage = Math.floor(response.totalResults/this.resultsPerPage);
-        if(response.totalResults%this.resultsPerPage!=0){
-          this.maxPage++;
-          console.log(this.maxPage);
-        } 
+        console.log(this.newsList);
       }, error => {
         console.log(error);
       }
@@ -66,7 +37,6 @@ export class NewsPage implements OnInit {
   }
 
   ionViewDidEnter () {
-    this.loadingEffect();
     this.loadPage();
   }
 
