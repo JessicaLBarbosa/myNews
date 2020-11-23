3f54b83d53cf33cb31ad674437a206b3357fe66a
Jesse <lbarbosa.jessica@gmail.com>
2020-11-23 18:28:02 -0300

    api, loading e refresh effects
    api do google news funcionando,
    efeitos de loading e refresh feitos

--- src/app/googlenews/news.service.ts
+++ src/app/googlenews/news.service.ts
@@ -6,13 +6,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NewsService {
 
-  //private key = "4976c948061a491fbb8dd1a2dcf885c0";
+  //private key:string = "4976c948061a491fbb8dd1a2dcf885c0";
   //private caminhoPadrao:string = "https://newsapi.org/v2";
 
   constructor(public http:HttpClient) { 
   }
   public getTopHeadlines (country="us") {
-    let news = "https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=4976c948061a491fbb8dd1a2dcf885c0";
+    let news = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=4976c948061a491fbb8dd1a2dcf885c0";
     return this.http.get(news);
   }
 }
--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -1,22 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
+import { NewsService } from 'src/app/googlenews/news.service';
 import { AutenticacaoService } from 'src/app/usuario/autenticacao.service';
 
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-news',
   templateUrl: './news.page.html',
   styleUrls: ['./news.page.scss'],
-
+  providers:[NewsService]
 })
 export class NewsPage implements OnInit {
 
   constructor(
     public autenticacaoService: AutenticacaoService,
     public router: Router,
+    public newsService:NewsService,
+    public loadingController: LoadingController
   ) { }
 
+  public lista_news = new Array<any>();
+  public page:number = 1;
+
+  loadPage () {
+    this.newsService.getTopHeadlines('us').subscribe (
+      data => {
+        const response = (data as any);
+        if (this.page == 1){
+          this.lista_news = response.articles;
+        } else {
+          this.lista_news = this.lista_news.concat(response.articles);
+        }
+        console.log(this.lista_news);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  async loadingEffect(){
+    const loading = await this.loadingController.create({
+      spinner: "dots",
+      message: 'Carregando notícias',
+      duration: 1000
+    });
+    await loading.present();
+    const { role, data } = await loading.onDidDismiss();
+  }
+
+  refreshEffect (event) {
+    this.page = 1;
+    this.loadPage();
+    console.log('Iniciando operação assíncrona');
+
+    setTimeout (() => {
+      event.target.complete();
+      console.log('Finalizando refresh');
+    }, 500);
+  }
+
+
   ionViewDidEnter () {
+    this.loadingEffect();
+    this.loadPage();
   }
 
   ngOnInit() {
