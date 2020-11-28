f260f92e6312dc2ce9075824f42aa3b5026dfae9
Jesse <lbarbosa.jessica@gmail.com>
2020-11-28 10:29:50 -0300

    Update news.service.ts

--- src/app/googlenews/news.service.ts
+++ src/app/googlenews/news.service.ts
@@ -9,7 +9,7 @@ export class NewsService {
   constructor(public http:HttpClient) { 
   }
   public getTopHeadlines (country="us", page=1) {
-    let news = "https://newsapi.org/v2/top-headlines?country=us&page=${this.page}&category=technology&apiKey=4976c948061a408fbb8dd1a2dcf885c0";
+    let news = "https://newsapi.org/v2/top-headlines?country=us&page=${this.page}&category=technology&apiKey=4976c948061a491fbb8dd1a2dcf885c0";
     return this.http.get(news);
   }
 }
